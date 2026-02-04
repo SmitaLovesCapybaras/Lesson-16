53c359e87c30255d4720b012169ae3df06c38c59
SmitaLovesCapybaras <Baggiapari@gmail.com>
2026-02-04 18:20:44 -0500

    Add files via upload

---
+++ Addtion.py
@@ -0,0 +1,12 @@
+print("Addition of two numbers")
+print("***********************")
+x=int(input("Enter the x value:"))
+y=int(input("Enter the y value:"))
+z=x+y
+print("Total:", z)
+
+print("...................")
+print("Hi my name is Smita")
+print("...................")
+
+  
